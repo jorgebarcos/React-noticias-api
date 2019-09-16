89a9bf96f1e6410c4325622dd1ed132e6d6718dd
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-16 12:48:48 -0300

    Documentando la Appa

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Formulario extends Component {
 	state = {
@@ -38,5 +39,9 @@ class Formulario extends Component {
 		);
 	}
 }
+
+Formulario.propTypes = {
+	consultarNoticias: PropTypes.func.isRequired
+};
 
 export default Formulario;
--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Noticia = ({ noticia }) => {
 	// extraer los datos
@@ -30,4 +31,7 @@ const Noticia = ({ noticia }) => {
 	);
 };
 
+Noticia.propTypes = {
+	noticia: PropTypes.object.isRequired
+};
 export default Noticia;
