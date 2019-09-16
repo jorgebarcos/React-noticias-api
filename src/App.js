e88c2bb32eaa0008d1fed79d51c82fab2c6bb960
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-16 11:39:30 -0300

    Consultando la API de noticias

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-	state = {};
+	state = {
+		noticias: []
+	};
+
+	componentDidMount() {
+		this.consultarNoticias();
+	}
+
+	consultarNoticias = async () => {
+		const url = `https://newsapi.org/v2/top-headlines?country=ar&category=business&apiKey=e8f71163bb6340cc86dd69f051bc0b7c`;
+
+		const respuesta = await fetch(url);
+		const noticias = await respuesta.json();
+
+		this.setState({
+			noticias: noticias.articles
+		});
+	};
+
 	render() {
 		return <h1>Noticias API React</h1>;
 	}
